1dca43e4b9489271bd36a9738eafe497fb32c641
Larisa Turetskaia <lturetskaja@gmail.com>
2025-10-12 22:38:37 +0100

    Add active styling for navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import NavB from "react-bootstrap/Navbar";
+import { NavLink, Link } from "react-router";
 
 export default function Navbar() {
   return (
@@ -9,19 +10,23 @@ export default function Navbar() {
         <NavB.Toggle aria-controls="basic-NavB-nav" />
         <NavB.Brand href="/">GrowPlants</NavB.Brand>
         <NavB.Collapse id="basic-NavB-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/products/house-plants">House Plants</Nav.Link>
-            <Nav.Link href="/products/garden-plants">Garden Plants</Nav.Link>
-            <Nav.Link href="/products/pots-and-planters">
+          <Nav className="me-auto" variant="underline">
+            <Nav.Link as={NavLink} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/products/house-plants">
+              House Plants
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/products/garden-plants">
+              Garden Plants
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/products/pots-and-planters">
               Pots & Planters{" "}
             </Nav.Link>
           </Nav>
         </NavB.Collapse>
         <NavB.Brand href="/">
-          <img
-            className="nav-icon"
-            src="/shopping-cart-icon.svg"
-          />
+          <img className="nav-icon" src="/shopping-cart-icon.svg" />
         </NavB.Brand>
       </Container>
     </NavB>
