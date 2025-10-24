233a289b056f71d75c521f3d57bc53b46a9f8579
Larisa Turetskaia <lturetskaja@gmail.com>
2025-10-24 14:13:21 +0100

    Add layout and styling for product page

--- app/routes/product.tsx
+++ app/routes/product.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "../+types/root";
 import { useAppSelector } from "store/hooks";
 import type { RootState } from "store/reduxStore";
+import { Carousel, Button } from "react-bootstrap";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -20,9 +21,11 @@ export default function Product({ params }: Route.ComponentProps) {
 
   if (
     productCategory === "house-plants" ||
-    productCategory === "house-plants"
+    productCategory === "garden-plants"
   ) {
-    product = useAppSelector((state: RootState) => state.plantProducts).find((product) => product.id === productId);
+    product = useAppSelector((state: RootState) => state.plantProducts).find(
+      (product) => product.id === productId
+    );
   } else if (productCategory === "pots-and-planters") {
     product = useAppSelector(
       (state: RootState) => state.potsPlantersProducts
@@ -38,9 +41,45 @@ export default function Product({ params }: Route.ComponentProps) {
 
   return (
     <>
-      <h2>{product.name}</h2>
-      <img src={imagePath} alt={altText} />
-      <p>{product.description} </p>
+      <section className="product-main">
+        <div className="product-gallery">
+          <Carousel data-bs-theme="dark">
+            <Carousel.Item>
+              <img src={imagePath} alt={altText} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={imagePath} alt={altText} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={imagePath} alt={altText} />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div className="product-info">
+          <h2>{product.name}</h2>
+          <div className="product-controls">
+
+            <div className="add-to-cart-btngroup">
+              <div className="quantity-input">
+                <button> &ndash;</button>
+                <input type="number" min='0' max='product.quanityt' step="1" />
+                <button>+</button>
+              </div>
+              <Button
+                variant="outline-success"
+                className={product.quantity > 0 ? "" : "disabled"}
+              >
+                {product.quantity > 0 ? "Add to cart" : "Out of stock"}
+              </Button>
+            </div>
+            <img className='like-btn' src="/heart-like-icon.svg" alt="Save button" />
+
+          </div>
+        </div>
+      </section>
+      <section className="product-details">
+        <p>{product.description} </p>
+      </section>
     </>
   );
 }
