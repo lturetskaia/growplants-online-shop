69f24ec8f6a9efa9e3b897f5c0b8235691570f88
Larisa Turetskaia <lturetskaja@gmail.com>
2025-10-25 01:27:29 +0100

    Add accordion  and dumy rating info for product page

--- app/routes/product.tsx
+++ app/routes/product.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "../+types/root";
 import { useAppSelector } from "store/hooks";
 import type { RootState } from "store/reduxStore";
-import { Carousel, Button } from "react-bootstrap";
+import { Carousel, Button, Accordion } from "react-bootstrap";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -56,37 +56,98 @@ export default function Product({ params }: Route.ComponentProps) {
           </Carousel>
         </div>
         <div className="product-info">
-          <h2>{product.name}</h2>
-          <div className="product-controls">
-            <div className="add-to-cart-btngroup">
-              <div className="quantity-input">
-                <button> &ndash;</button>
-                <input
-                  type="number"
-                  min="1"
-                  max="product.quanity"
-                  defaultValue="1"
-                  step="1"
-                />
-                <button>+</button>
-              </div>
-              <Button
-                variant="outline-success"
-                className={product.quantity > 0 ? "" : "disabled"}
-              >
-                {product.quantity > 0 ? "Add to cart" : "Out of stock"}
-              </Button>
+          <div>
+            <h2>{product.name}</h2>
+            <div className="rating-info">
+              <img
+                className="rating-img"
+                src="/stars.png"
+                alt="Five star rating"
+              />
+              <span>0 reviews</span>
             </div>
-            <img
-              className="like-btn"
-              src="/heart-like-icon.svg"
-              alt="Save button"
-            />
+          </div>
+          <div>
+            <p className="product-price left">
+              {product.price.toFixed(2)} &pound;
+            </p>
+            <div className="product-controls">
+              <div className="add-to-cart-btngroup">
+                <div className="quantity-input">
+                  <button> &ndash;</button>
+                  <input
+                    type="number"
+                    min="1"
+                    max="product.quanity"
+                    defaultValue="1"
+                    step="1"
+                  />
+                  <button>+</button>
+                </div>
+                <Button
+                  variant="outline-success"
+                  className={product.quantity > 0 ? "" : "disabled"}
+                >
+                  {product.quantity > 0 ? "Add to cart" : "Out of stock"}
+                </Button>
+              </div>
+              <img
+                className="like-btn"
+                src="/heart-like-icon.svg"
+                alt="Save button"
+              />
+            </div>
           </div>
         </div>
       </section>
+
       <section className="product-details">
-        <p>{product.description} </p>
+        <Accordion defaultActiveKey="0" alwaysOpen>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>About the plant</Accordion.Header>
+            <Accordion.Body>
+              {" "}
+              <p>{product.description} </p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Delivery</Accordion.Header>
+            <Accordion.Body>
+              {" "}
+              <p>
+                <span>Our Plant Nursery</span>: Our plants are nurtured and
+                prepared for dispatch in our tropical glasshouse nursery, where
+                we maintain an average temperature of 19°C. This ensures optimal
+                conditions for plant health.
+              </p>
+              <p>
+                <span>Packaging:</span> We pack the plants using custom-designed
+                boxes with special compartments and supports to prevent movement
+                and damage, so your plants arrive in perfect condition. Our
+                plant boxes are 100% recyclable, so they can be responsibly
+                disposed of without harming the planet.
+              </p>
+              <p>
+                <span>Quick delivery:</span> The standard delivery time for our
+                plants is 2-3 days. You can also choose next day delivery with
+                the same fee for any amount of plants & accessories.
+              </p>
+              <p>
+                <span>Plastic Nursery Pots:</span> All our plants are supplied
+                in practical plastic nursery pots. If you prefer a more
+                decorative option, we offer a range of stylish pots available
+                for purchase separately.
+              </p>
+              <p>
+                <span>Plant Pot Sizes:</span> Pot sizes may vary by +/- 1cm. To
+                ensure a proper fit, we recommend selecting a decorative
+                cachepot that is 1-2cm larger than the nursery pot size. For
+                instance, if you purchase a plant in a 15cm pot, a 16 - 17cm
+                decorative pot would be ideal.
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </section>
     </>
   );
