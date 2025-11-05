8134fd4081bee6aff5462c680da64566abb2ad2d
Larisa Turetskaia <lturetskaja@gmail.com>
2025-11-05 22:46:47 +0000

    Add cart badge logic

--- features/Cart/CartBadge.tsx
+++ features/Cart/CartBadge.tsx
@@ -1,7 +1,20 @@
+import { useAppSelector } from "store/hooks";
+
 export default function CartBadge() {
+  const cartData = useAppSelector((state) => state.cart);
+  const totalQuantity = cartData.reduce(
+    (prevQuantity, currItem) => prevQuantity + currItem.quantity,
+    0
+  );
+  let visibityClass = "";
+
+  if (totalQuantity === 0) {
+    visibityClass = "visually-hidden";
+  }
+
   return (
-    <div id="cart-badge">
-      <p>54</p>
+    <div id="cart-badge" className={visibityClass}>
+      <p>{totalQuantity}</p>
       <span className="visually-hidden">products in your cart.</span>
     </div>
   );
