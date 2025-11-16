78b562e634dfdb546fffd79112732bf0bfb2986f
Larisa Turetskaia <lturetskaja@gmail.com>
2025-11-16 01:18:17 +0000

    Fix delete button with overlay

--- features/Cart/CartItem.tsx
+++ features/Cart/CartItem.tsx
@@ -57,15 +57,12 @@ export default function CartItem({ cartItem }: { cartItem: CartItem }) {
     }
   }
 
+  const cartOverlayClass =
+    productQuantity === 0 ? "cart-item-overlay" : "cart-item-overlay-hidden";
+
   return (
     <li className="cart-item">
-      <div
-        className={
-          productQuantity === 0
-            ? "cart-item-overlay"
-            : "cart-item-overlay-hidden"
-        }
-      ></div>
+      <div className={cartOverlayClass}></div>
       <div>
         <Link to={urlPath}>
           <img src={imgPath} alt={altText} />
@@ -96,7 +93,7 @@ export default function CartItem({ cartItem }: { cartItem: CartItem }) {
       <div className={productQuantity === 0 ? "strikethrough" : ""}>
         &pound;{(cartItem.quantity * productPrice).toFixed(2)}
       </div>
-      <div>
+      <div className="delete-btn-div">
         <Button
           className="btn btn-outline-success delete-btn"
           onClick={handleDeleteCartItem}
