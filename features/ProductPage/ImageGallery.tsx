4dcef6392df32cb9b7c19d49f270eed6fd76908e
Larisa Turetskaia <lturetskaja@gmail.com>
2025-11-22 01:19:03 +0000

    Add common image to image gallery

--- features/ProductPage/ImageGallery.tsx
+++ features/ProductPage/ImageGallery.tsx
@@ -1,10 +1,10 @@
 import { Carousel } from "react-bootstrap";
 import type { ProductItem } from "common/types";
 
-export default function ImageGallery({product}: {product:ProductItem}) {
+export default function ImageGallery({ product }: { product: ProductItem }) {
   const imagePath = `/products/${product.category}/${product.image}`;
   const altText = `The image of ${product.name}`;
-  
+
   return (
     <div className="product-gallery">
       <Carousel>
@@ -15,7 +15,10 @@ export default function ImageGallery({product}: {product:ProductItem}) {
           <img src={imagePath} alt={altText} />
         </Carousel.Item>
         <Carousel.Item>
-          <img src={imagePath} alt={altText} />
+          <img
+            src={`/common/plant-size.png`}
+            alt={altText}
+          />
         </Carousel.Item>
       </Carousel>
     </div>
