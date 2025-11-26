d8db7da49e67febb02fda322621661aa1ec3d4ff
Larisa Turetskaia <lturetskaja@gmail.com>
2025-11-26 23:40:49 +0000

    Add generation of image paths for the imageGallery

--- features/ProductPage/ImageGallery.tsx
+++ features/ProductPage/ImageGallery.tsx
@@ -1,19 +1,23 @@
 import { Carousel } from "react-bootstrap";
 import type { ProductItem } from "common/types";
+import { constructAltText, constructImgPaths } from "common/helperFunctions";
 
 export default function ImageGallery({ product }: { product: ProductItem }) {
-  const imagePath = `/products/${product.category}/${product.image}`;
-  const altText = `The image of ${product.name}`;
+  const imagePaths = constructImgPaths(product.category, product.image);
+  const altText = constructAltText(product.name);
 
   return (
     <div className="product-gallery">
       <Carousel>
-        <Carousel.Item>
+        {imagePaths.map(img => <Carousel.Item key={img}>
+          <img src={img} alt={altText} />
+        </Carousel.Item>)}
+        {/* <Carousel.Item>
           <img src={imagePath} alt={altText} />
-        </Carousel.Item>
-        <Carousel.Item>
+        </Carousel.Item> */}
+        {/* <Carousel.Item>
           <img src={imagePath} alt={altText} />
-        </Carousel.Item>
+        </Carousel.Item> */}
         <Carousel.Item>
           <img
             src={`/common/plant-size.png`}
