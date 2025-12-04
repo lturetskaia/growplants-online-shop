734b22bdd95bbf962273211fe87596dbd72c8bc1
Larisa Turetskaia <lturetskaja@gmail.com>
2025-12-04 12:29:38 +0000

    Fix  menu items and display in mobile view

--- features/Navbar.tsx
+++ features/Navbar.tsx
@@ -1,41 +1,58 @@
 import Container from "react-bootstrap/cjs/Container.js";
 import Nav from "react-bootstrap/cjs/Nav.js";
 import NavB from "react-bootstrap/cjs/Navbar.js";
-import { NavLink} from "react-router";
+import { NavLink } from "react-router";
 import CartBadge from "./Cart/CartBadge";
 
 export default function Navbar() {
   return (
-    <NavB expand="lg" className="bg-body-tertiary">
+    <NavB collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <NavB.Toggle aria-controls="basic-NavB-nav" />
-        <NavB.Brand href="/">
-          <img src="/icons/growplants-icon.png" className="nav-icon"/>
+        <NavB.Brand href="/" id="nav-brand">
+          <img src="/icons/growplants-icon.png" className="nav-icon" />
           <span>GrowPlants</span>
         </NavB.Brand>
-        <NavB.Collapse id="basic-NavB-nav">
+        <NavB.Toggle aria-controls="responsive-navbar-nav" />
+        <NavB.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto" variant="underline">
-            <Nav.Link as={NavLink} to="/">
+            <Nav.Link as={NavLink} to="/" href="/">
               Home
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/products/house-plants">
+            <Nav.Link
+              as={NavLink}
+              to="/products/house-plants"
+              href="/products/house-plants"
+            >
               House Plants
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/products/garden-plants">
+            <Nav.Link
+              as={NavLink}
+              to="/products/garden-plants"
+              href="/products/garden-plants"
+            >
               Garden Plants
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/products/pots-and-planters">
+            <Nav.Link
+              as={NavLink}
+              to="/products/pots-and-planters"
+              href="/products/pots-and-planters"
+            >
               Pots & Planters{" "}
+            </Nav.Link>
+            <Nav.Link as={NavLink} to={"/cart"} href="/cart">
+              My Cart
             </Nav.Link>
           </Nav>
         </NavB.Collapse>
+        <NavB.Brand href="/" id="nav-brand-mobile">
+          <img src="/icons/growplants-icon.png" className="nav-icon" />
+          <span>GrowPlants</span>
+        </NavB.Brand>
         <div id="cart-icon-div">
-          <NavB.Brand>
-          <NavLink to={"/cart"}>
+          <Nav.Link as={NavLink} to={"/cart"} href="/cart">
             <img className="nav-icon" src="/icons/shopping-cart-icon.svg" />
-          </NavLink>
-          <CartBadge />  
-          </NavB.Brand>
+          </Nav.Link>
+          <CartBadge />
         </div>
       </Container>
     </NavB>
